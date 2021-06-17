6dc99cb24481d242f37e3c0e8db7fe3d27cb56da
swapno <swapno5896@yahoo.com>
2021-06-16 20:43:11 -0700

    project structer is almost complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from "react";
-import NavBar from "../src/components/Home/NavBar/NavBar";
-import TopBanner from "../src/components/Home/TopBanner/TopBanner";
-import Benefits from "../src/components/Home/Benefits/Benefits";
-import CurierService from "../src/components/Home/CurierService/CurierService";
-import FastCurrier from "../src/components/Home/FastCurrier/FastCurrier";
-import Footer from "../src/components/Home/Footer/Footer";
-import WorkingStyle from "../src/components/Home/WorkingStyle/WorkingStyle";
-import Ques from "../src/components/Home/Ques/Ques";
+import MainHome from "../src/components/Home/MainHome/MainHome";
 const App = () => {
   return (
     <div>
-      <NavBar></NavBar>
-      <TopBanner></TopBanner>
-      <Ques></Ques>
-      <Benefits></Benefits>
-      <CurierService></CurierService>
-      <FastCurrier></FastCurrier>
-      <WorkingStyle></WorkingStyle>
-      <Footer></Footer>
+      <MainHome></MainHome>
     </div>
   );
 };
---
+++ src/components/Home/MainHome/MainHome.js
@@ -0,0 +1,26 @@
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+
+import TopBanner from "../TopBanner/TopBanner";
+import Benefits from "../Benefits/Benefits";
+import CurierService from "../CurierService/CurierService";
+import FastCurrier from "../FastCurrier/FastCurrier";
+import Footer from "../Footer/Footer";
+import WorkingStyle from "../WorkingStyle/WorkingStyle";
+import Ques from "../Ques/Ques";
+const MainHome = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <TopBanner></TopBanner>
+      <Ques></Ques>
+      <Benefits></Benefits>
+      <CurierService></CurierService>
+      <FastCurrier></FastCurrier>
+      <WorkingStyle></WorkingStyle>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainHome;
